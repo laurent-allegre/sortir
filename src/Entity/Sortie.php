4eda5e409485c64de52e588b990c70fbe0fffc35
yohqui <yohqui@gmail.com>
2022-04-11 17:00:40 +0200

    modif entité sortie / text iso string

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -43,7 +43,7 @@ class Sortie
     private $nbInscriptionMax;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="text", nullable=true)
      */
     private $infosSortie;
 
