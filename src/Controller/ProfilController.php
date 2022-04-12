80a1d5a5817d6c940393b1263ac40415940590c4
monikasalmani <monika.salmani2021@campus-eni.fr>
2022-04-12 15:56:28 +0200

    ajout Mon Profil

---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Participant;
+use App\Form\ProfilType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfilController extends AbstractController
+{
+
+    /**
+     * @Route("/profil", name="app_profil")
+     */
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $profil = new Participant();
+
+
+        $profilForm = $this->createForm(ProfilType::class, $profil);
+        $profilForm->handleRequest($request);
+
+        if ($profilForm->isSubmitted()) {
+            $entityManager->persist($profil);
+            $entityManager->flush();
+            //return $this->redirectToRoute('profil/index.html.twig', ['id' => $profil->getId()]);
+        }
+
+
+        return $this->render('profil/index.html.twig', [
+            'profilForm' => $profilForm->createView()
+        ]);
+    }
+}
---
+++ src/Form/ProfilType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\PasswordHasher\Hasher\CheckPasswordLengthTrait;
+
+
+class ProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo', TextType::class, ['label' => 'Pseudo'])
+            ->add('prenom')
+            ->add('nom')
+            ->add('telephone')
+            ->add('email')
+            ->add('password')
+            //->add('confirmation')
+            ->add('avatar');
+
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
