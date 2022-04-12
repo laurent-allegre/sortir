7ae5b33a4585813b11b98373551a0dba390b295f
yohqui <yohqui@gmail.com>
2022-04-12 12:07:13 +0200

    modif main controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/index", name="index")
      */
     public function index(): Response
     {
