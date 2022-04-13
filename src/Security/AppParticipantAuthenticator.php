ea4495b433a8f1ebcdcb89b0f8781ef97fe73db9
laurent.allegre <laurent.allegre1@gmail.com>
2022-04-13 12:07:36 +0200

    Redirection du login

--- src/Security/AppParticipantAuthenticator.php
+++ src/Security/AppParticipantAuthenticator.php
@@ -49,9 +49,7 @@ class AppParticipantAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_profil'));
     }
 
     protected function getLoginUrl(Request $request): string
