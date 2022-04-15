c161326d0b17b4813d3ff36573c73de9bd961332
monikasalmani <monika.salmani2021@campus-eni.fr>
2022-04-15 15:06:28 +0200

    mise a jour annotation Route et suppression show.html.twig

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -10,9 +10,12 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route("/profil")
+ */
 class ProfilController extends AbstractController
 {
 
@@ -38,17 +41,15 @@ class ProfilController extends AbstractController
         ]);
     } */
     /**
-     * @Route("/profil", name="app_profil", methods={"GET", "POST"})
+     * @Route("/", name="app_profil", methods={"GET", "POST"})
      */
-    public function edit(Request $request,EntityManagerInterface $em,UserPasswordHasherInterface $hasher): Response
+    public function edit(Request $request,EntityManagerInterface $em): Response
     {
-        $this->hasher = $hasher;
         $participant=$this->getUser();
 
         $profilForm = $this->createForm(ProfilType::class, $participant)->handleRequest($request);
         if ($profilForm->isSubmitted() && $profilForm->isValid()) {
             $participant = $profilForm->getData();
-
             $em->persist($participant);
             $em->flush();
 
